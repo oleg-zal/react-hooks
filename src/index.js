cb8c7e8b5870f8410757c441aa815e1138946fb2
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-10-03 11:03:28 +0200

    Install router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Routes from 'routes'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return (
+    <div className="App">
+      <h3>Welcome here is our app</h3>
+      <Router>
+        <Routes />
+      </Router>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))

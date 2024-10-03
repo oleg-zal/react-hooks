e5139b55cab571a309a2950a5c008282c48e5bb6
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-10-03 11:28:24 +0200

    router

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import Routes from 'routes'
+import Topbar from 'components/topbar'
 
 const App = () => {
   return (
-    <div className="App">
-      <h3>Welcome here is our app</h3>
-      <Router>
-        <Routes />
-      </Router>
-    </div>
+    <Router>
+      <Topbar />
+      <Routes />
+    </Router>
   )
 }
 
